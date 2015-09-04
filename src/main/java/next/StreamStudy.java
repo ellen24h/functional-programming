89f5b4b87502b45bedbb74037d967dc11bba4b6c
ellen24h <ellen24h@gmail.com>
2015-09-04 15:16:24 +0900

    #Solved except last assignment

--- src/main/java/next/StreamStudy.java
+++ src/main/java/next/StreamStudy.java
@@ -5,6 +5,7 @@ import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.Arrays;
+import java.util.Comparator;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -12,21 +13,20 @@ public class StreamStudy {
 
 	public static long countWords() throws IOException {
 		String contents = new String(Files.readAllBytes(Paths
-				.get("src\\main\\resources\\war-and-peace.txt")), StandardCharsets.UTF_8);
+				.get("src/main/resources/war-and-peace.txt")), StandardCharsets.UTF_8);
 		List<String> words = Arrays.asList(contents.split("[\\P{L}]+"));
 
-		long count = 0;
-		for (String w : words) {
-		  if (w.length() > 12) count++;  
-		}
+		long count = words.stream().filter(w -> w.length() > 12).count();
 		return count;
 	}
 	
 	public static void printLongestWordTop100() throws IOException {
 		String contents = new String(Files.readAllBytes(Paths
-				.get("src\\main\\resources\\war-and-peace.txt")), StandardCharsets.UTF_8);
+				.get("src/main/resources/war-and-peace.txt")), StandardCharsets.UTF_8);
 		List<String> words = Arrays.asList(contents.split("[\\P{L}]+"));
 		
+		words.stream().filter(w -> w.length() > 12).sorted(Comparator.comparing(String::length)).distinct().limit(100).forEach(s -> System.out.println(s.toLowerCase()));
+		//Collections.sort(yourStringList, new LengthFirstComparator());
 		// TODO 이 부분에 구현한다.
 	}
 
@@ -39,6 +39,6 @@ public class StreamStudy {
 	}
 
 	public static long sumOverThreeAndDouble(List<Integer> numbers) {
-		return 0;
+		return numbers.stream().filter(n -> n > 3).map(n -> 2*n).collect(Collectors.toList()).stream().reduce(0, (x, y) -> x+y);
 	}
 }
